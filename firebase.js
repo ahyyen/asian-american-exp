31273336f269c4feff028aa88d35e4af2c859b36
ahyyen <allisonhyyen@gmail.com>
2025-08-02 23:16:26 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +13,9 @@ const firebaseConfig = {
   appId: "1:1060522188211:web:76d57a80c6c95ed264ffc6"
 };
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database();
 let dbRef = db.ref("text");
